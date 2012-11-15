33548dad8ca812b3c016cc9cd7635f8c6c6c6acf
Pragith <pragith@gmail.com>
2012-11-16 04:47:51 +0530

    Added code for MongoDB. Works!

--- mongodb/index.php
+++ mongodb/index.php
@@ -1,39 +1,45 @@
-<?php require_once('config.php'); 
+<?php 
 
-  $mode = "new";
-  $q = mysql_query("SELECT * FROM notes ORDER BY id DESC");
+$mode = "new";
 
-  $new = @$_POST['newNote'];
-  $edit = @$_POST['editBtn'];
-  $update = @$_POST['updateBtn'];
-  $delete = @$_POST['deleteBtn'];
+$new = @$_POST['newNote'];
+$edit = @$_POST['editBtn'];
+$update = @$_POST['updateBtn'];
+$delete = @$_POST['deleteBtn'];
 
-  $editNote = @$_POST['editNote'];
+$editNote = @$_POST['editNote'];
 
+// Connection
+$conn = new Mongo();
+$notes = $conn->praglabs_mongo->notes;
+
+// Get all the rows/collections
+$getNotes = $notes->find();
 
 // Create a note
   if ($new){
-    mysql_query("INSERT INTO notes VALUES ('','$new')");
-    header('Location: ./');
+    $notes->insert(array('note' => $new ));    
   }
   
 
 // Update a note
   if ($edit){
     $mode = "edit";
-    $noteToEdit = mysql_fetch_row(mysql_query("SELECT note FROM notes WHERE id = $edit"));
+    $id = new MongoID($edit);
+    $note = $notes->find(array('_id' => $id));
+    foreach ($note as $noteToEdit) {}
   }
 
   if ($update){
-      mysql_query("UPDATE notes SET note = '$editNote' WHERE id = $update");
-      header('Location: ./');
+    $id = new MongoID ($update);
+    $notes->update(   array( '_id'=> $id ),     array( 'note' => $editNote )    );
   }
 
   
 // Delete a note
   if ($delete){
-      mysql_query("DELETE FROM notes WHERE id = '$delete'");
-      header('Location: ./');
+    $id = new MongoID ($delete);
+    $notes->remove(array( '_id' => $id));
   }
 
 
@@ -58,8 +64,8 @@
     <div class="container-fluid">
       <form method="POST" class="form">
       
-        <div class="row-fluid">
-            
+        <div class="row-fluid">   
+                  
                   <div class="span6 offset3">
                   <h1>Notes</h1>  
                   <br>
@@ -67,7 +73,7 @@
                         <textarea class="span12" name="newNote" placeholder="Write a new note..."></textarea><br>
                         <input type="submit" class="btn btn-primary" value="Save"/>
                     <?php elseif ($mode == "edit"): ?>
-                          <textarea class="span12" name="editNote"><?= @$noteToEdit[0]; ?></textarea><br>
+                          <textarea class="span12" name="editNote"><?= $noteToEdit['note']; ?></textarea><br>
                           <button class="btn btn-success" name="updateBtn" value="<?= $edit ?>">Update</button>
                     <?php endif; ?>
                     <br><br>
@@ -79,24 +85,24 @@
      
                   <div class="span6 offset3">
                     <table class="table table-hover span">
-                    <?php    if (!$q):  ?>
+                    <?php    if (!$getNotes):  ?>
                           <tr>
                             <td>
                               You have not created any notes yet.
                             </td>
                           </tr>
                       <?php   else: 
-                        while ($note= @mysql_fetch_array($q)):  ?>
+                              foreach ($getNotes as $note):  ?>
                           <tr>
                             <td>
                                   <?= $note['note']; ?>
                             </td>
                             <td>
-                                 <button class="btn btn-inverse" name="editBtn" value="<?= $note['id'] ?>"><i class="icon-edit icon-white"></i></button>
-                                 <button class="btn btn-danger" name="deleteBtn" value="<?= $note['id'] ?>"><i class="icon-remove icon-white"></i></button>
+                                 <button class="btn btn-inverse" name="editBtn" value="<?= $note['_id'] ?>"><i class="icon-edit icon-white"></i></button>
+                                 <button class="btn btn-danger" name="deleteBtn" value="<?= $note['_id'] ?>"><i class="icon-remove icon-white"></i></button>
                             </td>
                           </tr>
-                          <?php endwhile; endif;    ?>
+                          <?php endforeach; endif;    ?>
                      </table>      
                     
                     </div>
